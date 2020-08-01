7f549782d86c590182dd7ae30129c558c2ca27d9
Himnish Sapkota <himnishsapkota@gmail.com>
2020-08-01 18:00:42 -0500

    Attempted to get keyboard working

--- snake.cpp
+++ snake.cpp
@@ -1,5 +1,8 @@
 #include "snake.h"
-
+#include <random>
+#include <iostream>
+#include "ncurses.h"
+using namespace std;
 void Snake::Setup(bool &gameOver)
 {
     gameOver = false;
@@ -33,6 +36,15 @@ void Snake::Draw()
             {
                 std::cout<< "|";
             }
+
+            if(i == y && j == x)
+            {
+                std::cout<<"O";
+            }
+            else if(i == fruitY && j == fruitX)
+            {
+                std::cout<<"F";
+            }
             else
             {
                 std::cout<<" ";
@@ -50,12 +62,75 @@ void Snake::Draw()
         std::cout<< "-";
     }
     std::cout<<'\n';
+    refresh();
 }
 void Snake::Input()
 {
+int ch;
+initscr();
+cbreak();
+noecho();
+scrollok(stdscr, TRUE);
+nodelay(stdscr, TRUE);
 
+for (;;)
+{
+    if ((ch = getch()) == ERR)
+    {
+        gameOver = true;
+    }
+    else
+    {
+        switch(ch)
+        { 
+            case 'a':
+                ungetch(ch);
+                direction_snake = LEFT;
+                break;
+            case 'd':
+                ungetch(ch);
+                direction_snake = RIGHT;
+                break;
+            case 'w':
+                ungetch(ch);
+                direction_snake = UP;
+                break;
+            case 's':
+                ungetch(ch);
+                direction_snake = DOWN;
+                break;
+            case 'q':
+                ungetch(ch);
+                direction_snake = UP;
+                gameOver = true;
+                break;
+        }
+    }
+}    
 }
+
 void Snake::Logic()
 {
+    switch(direction_snake)
+    {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+}
 
+bool Snake::getOver()
+{
+    return gameOver;
 }
