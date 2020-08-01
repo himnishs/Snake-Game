799275c6874ba77fc823aaf21e5b7e34b9cbb2e9
Himnish Sapkota <himnishsapkota@gmail.com>
2020-08-01 17:59:33 -0500

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "snake.h"
+#include "ncurses.h"
 int main()
 {
     Snake snake;
-    bool gameOver = false;
-    snake.Setup(gameOver);
+    bool gameOver_1 = snake.getOver();
+    snake.Setup(gameOver_1);
 
-    while(!gameOver)
+    while(!gameOver_1)
     {
+        gameOver_1 = snake.getOver();
+        refresh();
         snake.Draw();
-        //snake.Input();
-        //snake.Logic();
+        snake.Input();
+        snake.Logic();
         // Sleep function
     }
 
