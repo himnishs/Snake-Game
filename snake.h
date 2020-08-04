b7c835ac93eb3efd30b84ccaa52a77476fb4847d
Himnish Sapkota <himnishsapkota@gmail.com>
2020-08-04 17:27:47 -0500

    Added tail functionality

--- snake.h
+++ snake.h
@@ -1,6 +1,6 @@
 #ifndef SNAKE_H
 #define SNAKE_H
-
+#include <vector>
 const int width = 20;
 const int height = 20;
 class Snake
@@ -21,5 +21,9 @@ class Snake
         enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
         eDirection direction_snake;
         bool gameOver = false;
+        std::vector<int> tailX = std::vector<int>(100);
+        std::vector<int> tailY = std::vector<int>(100);
+        int nTail;
+        bool print = false;
 };
 #endif
